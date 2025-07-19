2e4244e80c25b63eca8a7a1c20bfaa4b46efee7e
maltidemo <150908958+Malticoder@users.noreply.github.com>
2025-07-19 17:26:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,9 +58,7 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
-}
+
 
 
 #output "Publi_Ip_Of_Ec2" {
