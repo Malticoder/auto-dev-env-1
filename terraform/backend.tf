4e9c471a57d8b309388e1b5fbd09045f330b63a7
maltidemo <150908958+Malticoder@users.noreply.github.com>
2025-07-20 14:33:57 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "malti-terraform-bucket-for-s3-backend"
-    key    = "statefile/test-wf-30/terraform.tfstate"
+    key    = "statefile/malti/terraform.tfstate"
     region = "us-east-1"
   }
 }
+
